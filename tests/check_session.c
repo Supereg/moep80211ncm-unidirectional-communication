9351cd259fee52861ebf5a0d8448666694f11230
Andreas Bauer <andi.bauer@tum.de>
2021-03-23 23:04:39 +0100

    Adding forwarding timeouts and await_fully_decoded()

--- tests/check_session.c
+++ tests/check_session.c
@@ -8,7 +8,9 @@
 // Created by Andreas Bauer on 22.02.21.
 //
 
+#include "check_session.h"
 #include "check_suites.h"
+
 #include "check_utils.h"
 
 #include <stdio.h>
@@ -154,7 +156,7 @@ START_TEST(test_session_coding_simple_two_nodes) {
     ret = session_encoder_add(source, CHECK_ETHER_TYPE, (u8*) example0, strlen(example0));
     ck_assert_int_eq(ret, EXIT_SUCCESS);
 
-    await_os_frame();
+    await_fully_decoded();
 
     // TODO shortcut to assert received packet
     received = peek_os_frame_entry(0);
@@ -165,7 +167,7 @@ START_TEST(test_session_coding_simple_two_nodes) {
     ret = session_encoder_add(source, CHECK_ETHER_TYPE, (u8*) example1, strlen(example1));
     ck_assert_int_eq(ret, EXIT_SUCCESS);
 
-    await_os_frame();
+    await_fully_decoded();
 
     received = peek_os_frame_entry(1);
     ck_assert_int_eq(received->length, strlen(example1));
@@ -176,7 +178,7 @@ START_TEST(test_session_coding_simple_two_nodes) {
     ret = session_encoder_add(source, CHECK_ETHER_TYPE, (u8*) example2, strlen(example2));
     ck_assert_int_eq(ret, EXIT_SUCCESS);
 
-    await_os_frame();
+    await_fully_decoded();
 
     received = peek_os_frame_entry(2);
     ck_assert_int_eq(received->length, strlen(example2));
@@ -187,7 +189,7 @@ START_TEST(test_session_coding_simple_two_nodes) {
 }
 END_TEST
 
-// TODO packet loss tests + randomized packets tests (+multiple session_encoder_add per await_os_frame())
+// TODO packet loss tests + randomized packets tests (+multiple session_encoder_add per await_fully_decoded())
 
 /* -------------------------------------------------------------------------------------------- */
 
@@ -212,4 +214,10 @@ Suite* session_suite() {
     suite_add_tcase(suite, session_coding);
 
     return suite;
+}
+
+/* ---------------- Internal session.c functionality exposed to unit test API ----------------- */
+
+struct list_head* session_generation_list(session_t* session) {
+    return &session->generations_list;
 }
