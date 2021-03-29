f67660d53c397ba1d3abad9c1f04371bad63f90f
Andreas Bauer <andi.bauer@tum.de>
2021-03-29 16:33:58 +0200

    Remove the old coded hdr

--- src/frametypes.h
+++ src/frametypes.h
@@ -8,12 +8,12 @@
 #include "generation.h"
 
 enum headertypes {
-	NCM_HDR_DATA	= MOEP_HDR_VENDOR_MIN, // 0x20
+	NCM_HDR_DATA = MOEP_HDR_VENDOR_MIN, // 0x20
 	NCM_HDR_CODED,
 	NCM_HDR_BCAST,
 	NCM_HDR_BEACON,
 	NCM_HDR_UNIDIRECTIONAL_CODED, // new unidir hdr type
-	NCM_HDR_INVALID	= MOEP_HDR_COUNT-1
+	NCM_HDR_INVALID = MOEP_HDR_COUNT - 1
 };
 
 enum frametypes {
@@ -35,43 +35,19 @@ struct ncm_beacon_payload {
 } __attribute__((packed));
 
 /**
-  Bware: the coding header is a variable-length header, depending on the galois
-  field and generation size.
-  */
-struct ncm_hdr_coded {
-	struct moep_hdr_ext hdr;
-	/// The session id consisting of sender and receiver mac address
-    /// (appended according to the definition of master and slave).
-	u8 sid[2*IEEE80211_ALEN];
-	/// The galois field type used. See `MOEPGF_TYPE`.
-	u8 gf:2;
-    /// Defines the generation window size. So the count of our generation list for the given session.
-	u8 window_size:6;
-	/// The generation sequence number of the generation this packet is sent for.
-	/// See `generation_seq`.
-	u16 seq;
-	/// Corresponds to the generation sequence number of the first generation
-	/// of the generation list for the given session. See `generation_lseq`.
-	u16 lseq;
-	/// One `generation_feedback` for every generation in our generation list.
-	/// Meaning the array equals to `window_size`.
-	// TODO removed: struct generation_feedback fb[0];
-} __attribute__((packed));
-
-/**
  * Header type NCM_HDR_UNIDIRECTIONAL_CODED
  */
 struct ncm_hdr_unidirectional_coded {
-    struct moep_hdr_ext hdr;
-    u8 session_id[2 * IEEE80211_ALEN];
+	struct moep_hdr_ext hdr;
+	u8 session_id[2 * IEEE80211_ALEN];
 	// The generation sequence number of the generation this packet is sent for.
-    u16 sequence_number;
+	u16 sequence_number;
 	// sequence number of the first generation in the window
-    u16 window_id;
-    u8 gf:2;
-	// acknowlegment flag
-    u8 ack:1;
-    u8 window_size:5;
+	u16 window_id;
+	u8 gf : 2;
+	// acknowledgment flag
+	u8 ack : 1;
+	u8 window_size : 5;
 };
 
 struct ncm_hdr_bcast {
